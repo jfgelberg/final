cc1bdf6fe83c1b4d7b8d1273c2096b89af2167b9
Javi Gelberg <128005377+jfgelberg@users.noreply.github.com>
2024-11-29 16:33:46 -0300

    Update ListadoPeliculas.jsx

--- DWT4AV-FRONT/src/components/Peliculas/ListadoPeliculas.jsx
+++ DWT4AV-FRONT/src/components/Peliculas/ListadoPeliculas.jsx
@@ -191,19 +191,40 @@ const ListadoPeliculas = ({ listado, usuarioId }) => {
                 )}
             </div>
 
+           {/* Paginación */}
             <div className="d-flex justify-content-center mt-4">
                 <nav>
                     <ul className="pagination">
                         <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
-                            <button className="page-link" onClick={() => paginate(currentPage - 1)}>&laquo; Anterior</button>
+                            <button
+                                className="page-link"
+                                onClick={() => paginate(currentPage - 1)}
+                                disabled={currentPage === 1} // Desactiva el botón si estás en la primera página
+                            >
+                                &laquo; Anterior
+                            </button>
                         </li>
                         {[...Array(totalPages)].map((_, index) => (
-                            <li key={index + 1} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
-                                <button className="page-link" onClick={() => paginate(index + 1)}>{index + 1}</button>
+                            <li
+                                key={index + 1}
+                                className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}
+                            >
+                                <button
+                                    className="page-link"
+                                    onClick={() => paginate(index + 1)}
+                                >
+                                    {index + 1}
+                                </button>
                             </li>
                         ))}
                         <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
-                            <button className="page-link" onClick={() => paginate(currentPage + 1)}>Siguiente &raquo;</button>
+                            <button
+                                className="page-link"
+                                onClick={() => paginate(currentPage + 1)}
+                                disabled={currentPage === totalPages} // Desactiva el botón si estás en la última página
+                            >
+                                Siguiente &raquo;
+                            </button>
                         </li>
                     </ul>
                 </nav>
